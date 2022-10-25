bd597e89495b28ba86e221d3b287d24bb005a59a
AsmaaElshalGBX <asmaa.elshal@gearbox.com>
2022-10-25 18:27:21 -0500

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+    public static class HelloWorld
+    {
+        public static void PrintHelloWorld()
+        {
+            Console.WriteLie("HGello World from com.npm.package.helloworld package .....");
+        }
+}
